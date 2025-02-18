964fe79b677a60bdcddf8bf2028a5faf85ba8e79
KiraBelak <kirabelak@kirabel.com>
2025-02-17 20:19:31 -0600

    first commit

---
+++ src/main/java/com/example/gestioncredito/GestioncreditoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestioncredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioncreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioncreditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestioncredito/GestioncreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestioncredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioncreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
