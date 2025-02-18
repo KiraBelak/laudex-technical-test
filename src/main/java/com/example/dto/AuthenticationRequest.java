72bbbab1c73ea1b19ef60ac214c0058fc8896842
KiraBelak <kirabelak@kirabel.com>
2025-02-18 09:51:50 -0600

    Add AuthenticationRequest DTO with validation for login credentials

---
+++ src/main/java/com/example/dto/AuthenticationRequest.java
@@ -0,0 +1,37 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class AuthenticationRequest {
+    @NotBlank(message = "El nombre de usuario es obligatorio")
+    private String username;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    @Size(min = 6, message = "La contraseña debe tener al menos 6 caracteres")
+    private String password;
+
+    public AuthenticationRequest() {
+    }
+
+    public AuthenticationRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
